e3e02c2988fff477f3b29f82bab660cc82b025a5
blancojohn <142819007+blancojohn@users.noreply.github.com>
2024-03-21 12:24:36 +0000

    Falta mostrar cartas en varias filas según cantidad

--- src/app.js
+++ src/app.js
@@ -5,24 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let números = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
+let números = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 let palos = ["♦", "♥", "♠", "♣"];
 let cartasDesordenadas = [];
-
+let orden = [];
 let botónDraw = document.querySelector("#draw");
 
 /* captura el valor pasado por el input */
@@ -30,13 +16,13 @@ function generaInputCartas() {
   cartasDesordenadas = [];
   let inputNumber = document.querySelector("#input-número");
   let inputNumberValue = inputNumber.value;
-  console.log("Input pasado", inputNumberValue);
+  console.log("Input pasado", inputNumberValue, "cartas");
 
   for (let i = 1; i <= inputNumberValue; i++) {
     cartasDesordenadas.push(generarCartas());
   }
 
-  console.log("Hay:", cartasDesordenadas, "cartas");
+  console.log(cartasDesordenadas, "cartas repartidas");
   return cartasDesordenadas;
 }
 
@@ -48,7 +34,6 @@ botónDraw.addEventListener("click", drawDesactivado, false);
 /* Dibuja las cartas */
 function generarCartas() {
   let mostrarCartas = document.querySelector("#mostrar-cartas");
-
   let carta = document.createElement("div");
   carta.classList.add("card");
 
@@ -95,20 +80,63 @@ function generarCartas() {
 
 botónDraw.addEventListener("click", generaInputCartas);
 
-/* function sort(arr = []) {
-  const len = arr.length;
+/* cartasDesordenadas = generarCartas(); */
+let botonSort = document.querySelector("#sort");
+function ordenaCartas() {
+  let cartasOrdenadas = document.querySelector("#cartas-ordenadas");
+  const len = cartasDesordenadas.length;
+  cartasOrdenadas.innerHTML = "";
+  console.log("ordenar", len, "cartas repartidas");
   for (let i = 0; i < len - 1; i++) {
     for (let j = 0; j < len - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+      if (cartasDesordenadas[j].número > cartasDesordenadas[j + 1].número) {
+        const temp = cartasDesordenadas[j];
+        cartasDesordenadas[j] = cartasDesordenadas[j + 1];
+        cartasDesordenadas[j + 1] = temp;
+      }
+    }
+
+    let contenedor = document.querySelector("#div");
+
+    for (let x = 0; x < len; x++) {
+      let cartaEnOrden = document.createElement("div");
+      cartaEnOrden.classList.add("card");
+
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      let paloSuperior = document.createElement("div");
+      paloSuperior.classList.add("position-absolute", "top-0", "start-0");
+      paloSuperior.innerHTML = cartasDesordenadas[x].palo;
+
+      let númeroCentral = document.createElement("div");
+      númeroCentral.innerHTML = cartasDesordenadas[x].número;
+
+      let paloInferior = document.createElement("div");
+      paloInferior.classList.add("position-absolute", "bottom-0", "end-0");
+      paloInferior.innerHTML = cartasDesordenadas[x].palo;
+
+      cartasOrdenadas.appendChild(cartaEnOrden);
+      cartaEnOrden.appendChild(cardBody);
+      cardBody.appendChild(paloSuperior);
+      cardBody.appendChild(númeroCentral);
+      cardBody.appendChild(paloInferior);
+
+      if (
+        cartasDesordenadas[x].palo == "♦" ||
+        cartasDesordenadas[x].palo == "♥"
+      ) {
+        paloSuperior.style.color = "red";
+        paloInferior.style.color = "red";
+        númeroCentral.style.color = "red";
+      } else {
+        paloSuperior.style.color = "black";
+        paloInferior.style.color = "black";
+        númeroCentral.style.color = "black";
       }
     }
   }
-  return arr;
+  console.log("cartas ordenadas", cartasDesordenadas);
 }
-let result = sort([...números]);
-console.log("Método Bubble Sorting", result);
-console.log("cartas sin ordenar", números);
- */
+
+botonSort.addEventListener("click", ordenaCartas);
